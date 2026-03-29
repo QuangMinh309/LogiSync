5b504216fa7eac961e216702f505133b2adac95f
QMinh <quangminhpham309@gmail.com>
2026-03-29 14:12:29 +0700

    ci(root): setup github actions workflow and project scripts

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,18 +1,36 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  // Khai báo các folder cần bỏ qua
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "dist/**",
+      "next-env.d.ts",
+      "node_modules/**",
+    ],
+  },
+
+  // Kế thừa cấu hình của Next.js
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Cấu hình riêng cho project (Nếu cần)
+  {
+    rules: {
+      // Ví dụ: "no-unused-vars": "warn"
+    },
+  },
+];
 
 export default eslintConfig;
